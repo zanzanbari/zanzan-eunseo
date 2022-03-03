d97fda6c81be9bd2e8a7f3d4e56505afe27afefb
eunseo5355 <eunseo5650525@gmail.com>
2022-03-04 02:26:23 +0900

    [Add] Asset, Color 추가

---
+++ KakaoT-Practice/KakaoT-Practice/Global/Constant/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  KakaoT-Practice
+//
+//  Created by 배은서 on 2022/03/04.
+//
+
+import UIKit
+
+struct Color {
+    static let black100 = UIColor(named: "black100")
+    static let black200 = UIColor(named: "black200")
+    static let navy100 = UIColor(named: "navy100")
+    static let navy200 = UIColor(named: "navy200")
+    static let navy300 = UIColor(named: "navy300")
+    static let navy400 = UIColor(named: "navy400")
+    static let gray100 = UIColor(named: "gray100")
+    static let gray200 = UIColor(named: "gray200")
+    static let gray300 = UIColor(named: "gray300")
+    static let gray400 = UIColor(named: "gray400")
+    static let gray500 = UIColor(named: "gray500")
+    static let white = UIColor(named: "white")
+    static let blue100 = UIColor(named: "blue100")
+    static let blue200 = UIColor(named: "blue200")
+    static let red100 = UIColor(named: "red100")
+    static let red200 = UIColor(named: "red200")
+}
